449936d025490884b14da4dbd90b237674808169
admin <admin@163.com>
2018-09-04 16:17:07 +0800

    下载修改

--- git-test/src/com/itheima/git/FirstClass.java
+++ git-test/src/com/itheima/git/FirstClass.java
@@ -8,9 +8,13 @@ public class FirstClass {
         System.out.println("hello git project");
         System.out.println("hello git project");
         System.out.println("hello git project");
+        sun();
+        //hello
 
     }
     public static void sun(){
         int i =10;
+        i+=100;
+        System.out.println(i);
     }
 }
